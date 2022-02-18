0e4c2e425a4b3fbd92fd342c3802b0d9ec8681fd
hien nguyen <thuhiennguyen198@gmail.com>
2022-02-18 17:19:33 -0500

    add message div

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const tiles = document.querySelector('.tile-container');
+const tiles = document.querySelector(".tile-container");
 // console.log("tiles", tiles)
-const keyboard = document.querySelector('.keyboard-container');
+const keyboard = document.querySelector(".keyboard-container");
+const messageDisplay = document.querySelector(".message-container");
 // console.log("keyboard", keyboard);
 //querySelector() returns the first Element within the document that matches the specified selector, or group of selectors.
+const WORDLE = "GUESS";
+
 const keys = [
   "Q",
   "W",
@@ -34,38 +37,37 @@ const keys = [
   "del",
 ];
 
-keys.forEach(key => {
+keys.forEach((key) => {
   const keyElement = document.createElement("button");
   keyElement.textContent = key;
-  keyElement.setAttribute('id', key);
+  keyElement.setAttribute("id", key);
   keyElement.addEventListener("click", () => handleClick(key));
   keyboard.append(keyElement); //inserts a set of Node objects or DOMString objects after the last child of the document
-})
+});
 
 const guessRows = [
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-]
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
 
 guessRows.forEach((row, rowIndex) => {
-let rowElement = document.createElement('div');
-rowElement.setAttribute('id', 'row-' + rowIndex);
+  let rowElement = document.createElement("div");
+  rowElement.setAttribute("id", "row-" + rowIndex);
 
-row.forEach((tile,tileIndex)=>{
-    const tileElement = document.createElement('div');
+  row.forEach((tile, tileIndex) => {
+    const tileElement = document.createElement("div");
     // tileElement.addEventListener('keydown', (e) => {
     //   console.log(e);
     // });
-    tileElement.setAttribute('id', 'row-'+rowIndex+'-tile-'+tileIndex);
+    tileElement.setAttribute("id", "row-" + rowIndex + "-tile-" + tileIndex);
     rowElement.append(tileElement);
-})
-tiles.append(rowElement)
-})
-
+  });
+  tiles.append(rowElement);
+});
 
 const handleClick = (letter) => {
   if (letter == "del") {
@@ -77,14 +79,13 @@ const handleClick = (letter) => {
     console.log("guessRows", guessRows);
     return;
   } else {
-     console.log("press", letter);
-     addLetter(letter);
+    console.log("press", letter);
+    addLetter(letter);
   }
- 
-}
+};
 let currentRow = 0;
 let currentTile = 0;
-const addLetter = (letter) => {  
+const addLetter = (letter) => {
   if (currentRow < 5 && currentTile < 6) {
     const node = document.getElementById(
       "row-" + currentRow + "-tile-" + currentTile
@@ -99,54 +100,51 @@ const addLetter = (letter) => {
 
 const deleteLetter = () => {
   currentTile--;
-   const node = document.getElementById(
-     "row-" + currentRow + "-tile-" + currentTile
-   );
-   node.textContent = '';
-   guessRows[currentRow][currentTile] = '';//tai sao can set cai nay, no khong giong line 104 sao?
-   node.setAttribute("data", "");//tai sao phai set data? sao khong that no hien len tren html file
-}
+  const node = document.getElementById(
+    "row-" + currentRow + "-tile-" + currentTile
+  );
+  node.textContent = "";
+  guessRows[currentRow][currentTile] = ""; //tai sao can set cai nay, no khong giong line 104 sao?
+  node.setAttribute("data", ""); //tai sao phai set data? sao khong that no hien len tren html file
+};
 
 const checkGuessRow = (currentRow) => {
-  if (currentRow.length < 5) {
-    console.log("Not enough word")
+  if (currentTile < 4) {
+    console.log("Not enough word");
   } else {
-    currentRow.forEach((char, charIndex) => {
-      if(currentRow[charIndex] == wordle[charIndex]){
-        console.log("flip the node to green")
-      } else if (currentRow[charIndex] !== wordle[charIndex] && currentRow[charIndex].includes(wordle)) {
-         console.log("flip the node to yellow")
+    const guessWord = currentRow.join('');
+      if (guessWord == WORDLE) {
+        showMessages(message);
       } else {
-         console.log("flip the node to gray")
+        flipNode(currentRow)
       }
-    }
   }
+};
 
+
+const showMessages = (message) => {
+  const messageElement = document.createAttribute('p');
+  messageElement.textContent = message;
+  messageDisplay.append(messageElement)
 }
-//  getUserInput() {
-//         rowElement.addEventListener("keydown", function (event) {
-//             event.stopPropagation(); // stop bubbling out 
-//             if (event.key === "Enter") {
-//                 for (let i = 0; i < this.text.length; i++) {
-//                     if (this.text[i] === userInput.value) {
-//                         this.score += 1;
-//                         setScore(this.score);
-//                         this.text[i] = undefined; //remove in this.text
-//                         userInput.value = '';
-//                         this.draw();
-//                     }
-//                 }
-//                 if (this.text.every(e => !e)) {
-//                     this.nextLevel();
-//                 }
-//             }
-//         }.bind(this));
-//     };
 
-
+const flipNode = (currentRow) => {
+  currentRow.forEach((char, charIndex) => {
+      if (char == WORDLE[charIndex]) {
+        console.log("flip the node to green");
+      } else if (
+        char != WORDLE[charIndex] &&
+        currentRow[charIndex].includes(wordle)
+      ) {
+        console.log("flip the node to yellow");
+      } else {
+        console.log("flip the node to gray");
+      }
+    });
+}
 //addLetter()
 //handlekeydown
 //lay letter tu keydown bo vao guessRow(1-6)
 //check neu no la meaning word(word dictionary api) thi so sanh guessword vs wordle
-//doi mau o neu no co chua letter nao cua tu khoa 
-//check game 
+//doi mau o neu no co chua letter nao cua tu khoa
+//check game
